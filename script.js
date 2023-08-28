5ad35c318055aae7f706679e755b099890f388b5
aashish_k <aashishkathat00@gmail.com>
2023-08-28 22:42:58 +0530

    populate the countdown UI

--- script.js
+++ script.js
@@ -1,20 +1,56 @@
 const inputContainer = document.querySelector('#input-container');
 const countdownForm = document.querySelector('#countdown');
+
 const dateEl = document.querySelector('#date-picker');
+const countdownEl = document.querySelector('#countdown');
+const countdownTitleEl = document.querySelector('#countdown-title');
+const timeElement = document.querySelectorAll('span');
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
 
 // Set Date input with today's date
 const today = new Date().toISOString().split('T')[0];
 
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
 
 // functions
+// populate countdown / complete ui
+function updateDOM() {
+  const now = new Date().getTime();
+  const distance = countdownValue - now;
+
+  const days = Math.floor(distance / day);
+  const hours = Math.floor((distance % day) / hour);
+  const minutes = Math.floor((distance % hour) / minute);
+  const seconds = Math.floor((distance % minute) / second);
+
+  //   Populating countdown element
+  countdownTitleEl.textContent = `${countdownTitle}`;
+  timeElement[0].textContent = `${days}`;
+  timeElement[1].textContent = `${hours}`;
+  timeElement[2].textContent = `${minutes}`;
+  timeElement[3].textContent = `${seconds}`;
+  //   Hides input container
+  inputContainer.hidden = true;
+  //   Show countdown
+  countdownEl.hidden = false;
+}
+
 // take values from input
 function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.srcElement[1].value;
-  console.log(e);
+
+  //   number version of current date
+  countdownValue = new Date(countdownDate).getTime();
+
+  updateDOM();
 }
 
 dateEl.setAttribute('min', today);
